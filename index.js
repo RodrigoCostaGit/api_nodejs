7b1569c05369f6e8911e26c1ace8acb79d03509d
Rodrigo <rodrigoafonsocostawork@gmail.com>
2021-11-12 00:08:09 +0000

    genres done

--- index.js
+++ index.js
@@ -5,6 +5,72 @@ const cheerio = require("cheerio");
 
 const app=express();
 
+const generos=[
+    {
+        name:"comedy",
+        url:"https://www.imdb.com/search/title/?genres=comedy",
+    },
+    {
+        name:"Sci-fi",
+        url:"https://www.imdb.com/search/title/?genres=sci-fi",
+    },
+    {
+        name:"Horror",
+        url:"https://www.imdb.com/search/title/?genres=Horror",
+    },
+    {
+        name:"Romance",
+        url:"https://www.imdb.com/search/title/?genres=Romance",
+    },
+    {
+        name:"Action",
+        url:"https://www.imdb.com/search/title/?genres=Action",
+    },
+    {
+        name:"Thriller",
+        url:"https://www.imdb.com/search/title/?genres=Thriller",
+    },
+    {
+        name:"Drama",
+        url:"https://www.imdb.com/search/title/?genres=Drama",
+    },
+    {
+        name:"Mystery",
+        url:"https://www.imdb.com/search/title/?genres=Mystery",
+    },
+    {
+        name:"crime" ,
+        url:"https://www.imdb.com/search/title/?genres=Crime" ,
+    },
+    {
+        name:"Animation" ,
+        url:"https://www.imdb.com/search/title/?genres=Animation",
+    },
+    {
+        name:"Adventure" ,
+        url:"https://www.imdb.com/search/title/?genres=Adventure",
+    },
+    {
+        name:"Fantasy" ,
+        url:"https://www.imdb.com/search/title/?genres=Fantasy",
+    },
+    {
+        name:"Comedy-romance",
+        url:"https://www.imdb.com/search/title/?genres=Comedy-romance",
+    },
+    {
+        name:"action-comedy" ,
+        url:"https://www.imdb.com/search/title/?genres=Action-comedy",
+    },
+    {
+        name:"Superhero",
+        url:"https://www.imdb.com/search/title/?genres=Superhero",
+    },
+
+
+]
+
+
 const movieList = []
 
 app.get("/",(req,res) => {
@@ -35,6 +101,17 @@ app.get("/topMovies",(req,res)=>{
         res.json(movieList)
 
     }).catch((err) => console.log(err))
+})
+
+
+app.get("/:nomeGenero",(req,res)=>{
+    const generoFilme = req.params.nomeGenero
+
+    const generoUrl = generos.filter(genero =>genero.name==generoFilme)[0].url
+    console.log("entrou em "+generoFilme)
+    const tempUrl = generoUrl + encodeURIComponent(Array)
+    res.json(tempUrl)
+
 })
 
 app.listen(PORT,() => console.log("server running on PORT ${PORT}"))
